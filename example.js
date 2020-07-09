2dfa55019b727b8ae52fabc28c364a7e1057af73
zxs1215 <ujjfvswc@sina.com>
2020-07-09 21:25:30 +0800

    修改了无法打开相册的bug

--- example.js
+++ example.js
@@ -1,13 +1,13 @@
 ﻿const urls = {
     loginPage: 'https://passport.weibo.cn/signin/login?entry=mweibo&res=wel&wm=3349&r=https%3A%2F%2Fm.weibo.cn%2F',
-    photo: 'http://photo.weibo.com/' + process.argv[2] + '/talbum/index#!/mode/3/page/'
+    photo: 'https://weibo.cn/album/albummblog/?rl=11&fuid=' + process.argv[2] + '&page='
 };
 
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 // 把下面改成你自己的用户名密码
 const acct = `*******@***.com`;
-const pass = `*********`;
+const pass = `********`;
 
 function wait(ms) {
     return new Promise(resolve => setTimeout(() => resolve(), ms));
@@ -35,39 +35,37 @@ async function main() {
     await wait(1000);
     
     await page.goto(urls.photo+1);
-    await page.waitForSelector("[class='photoList clearfix'] img");
-    await page.waitForSelector('ul .M_txtb');
-    //相册照片总数
-    var count = await page.evaluate(() => document.querySelector('.m_piclist ul li [title="微博配图"]').parentNode.parentNode.nextSibling.nextSibling.innerText);
-    count = count.replace(/[^0-9]/ig, "");
+    await page.waitForSelector("#pagelist");
+    //相册照片总页数
+    var str = await page.evaluate(() => document.querySelector('#pagelist').textContent);
+    var count = parseInt(str.substr(str.indexOf('/')+1));
     console.log(count);
     var all = [];
     var reget = 0;
-    for(i = 0;i<count;i+=32) {
-        var pindex = (i/32+1);
-        await page.goto(urls.photo+pindex);
+    for(i = 1;i<count;i++) {
+        await page.goto(urls.photo+i);i
         try {
-            await page.waitForSelector("[class='photoList clearfix'] img",{timeout : 5000});
+            await page.waitForSelector("#pagelist",{timeout : 5000});
         } catch (error) {
-            console.log('page' + pindex + ' error reopen this page');
-            i -= 32;
+            console.log('page' + i + ' error reopen this page');
+            i--;
             await page.close();
-            page = await browser.newPage();
+            page = await briowser.newPage();
             reget ++;
             if(reget>=10)
                 break;
             continue;
         }
         reget = 0;
-        console.log('page ' + pindex + ' get over')
-        var imgs = await page.evaluate(() => Array.from(document.querySelectorAll("[class='photoList clearfix'] img"), e => e.src));
+        console.log('page ' + i + ' get over')
+        var imgs = await page.evaluate(() => Array.from(document.querySelectorAll("img.c"), e => e.src));
         all = all.concat(imgs);
         await page.close();
         page = await browser.newPage();
         await page.setViewport({ width: 800, height: 600 })
     }
     console.log('get all pages start download images');
-    all = all.map(x => x.replace('square', 'large'));
+    all = all.map(x => x.replace('wap180', 'large'));
     var file = fs.createWriteStream('urls.txt');
     file.on('error', function(err) { /* error handling */ });
     all.forEach(x => file.write(x + '\n'));
